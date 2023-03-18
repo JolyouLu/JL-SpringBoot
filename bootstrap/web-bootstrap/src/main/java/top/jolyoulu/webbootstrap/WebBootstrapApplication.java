1f2ab371c9839cfb6487c0bf15b54be0cf65e142
Jolyoulu <1307633126@qq.com>
2023-03-18 13:15:27 +0800

    调整项目结构

--- bootstrap/web-bootstrap/src/main/java/top/jolyoulu/webbootstrap/WebBootstrapApplication.java
+++ bootstrap/web-bootstrap/src/main/java/top/jolyoulu/webbootstrap/WebBootstrapApplication.java
@@ -1,5 +1,4 @@
-package top.jolyoulu.bootstrap;
-
+package top.jolyoulu.webbootstrap;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -9,15 +8,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @Author: JolyouLu
+ * @Date: 2023/3/18 12:20
+ * @Description
+ */
 @EnableScheduling
 @EnableAspectJAutoProxy
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan("top.jolyoulu.**.dao")                  //Mapper扫描
 @ComponentScan(basePackages = {"top.jolyoulu.*"})   //Bean扫描
-public class BootstrapApplication {
-
+public class WebBootstrapApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BootstrapApplication.class, args);
+        SpringApplication.run(WebBootstrapApplication.class,args);
     }
-
 }
