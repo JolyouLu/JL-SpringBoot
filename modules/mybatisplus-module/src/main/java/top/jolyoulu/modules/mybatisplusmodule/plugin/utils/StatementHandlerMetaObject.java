07ddae1641dd840b80c5d8e8d1dfb27ed90a0c26
Jolyoulu <1307633126@qq.com>
2023-03-19 23:03:32 +0800

    mybatisplus-module 插件开发

--- modules/mybatisplus-module/src/main/java/top/jolyoulu/modules/mybatisplusmodule/plugin/utils/StatementHandlerMetaObject.java
+++ modules/mybatisplus-module/src/main/java/top/jolyoulu/modules/mybatisplusmodule/plugin/utils/StatementHandlerMetaObject.java
@@ -1,28 +1,65 @@
 package top.jolyoulu.modules.mybatisplusmodule.plugin.utils;
 
+import lombok.Data;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 
+@Data
 public class StatementHandlerMetaObject {
 
     private final MetaObject metaObject;
 
+    /**
+     * 转换为Mybatis的反射工具类
+     * @param statementHandler
+     */
     public StatementHandlerMetaObject(StatementHandler statementHandler) {
         this.metaObject = SystemMetaObject.forObject(statementHandler);
     }
 
+    /**
+     * 获取StatementHandler中的MappedStatement
+     * @return
+     */
     public MappedStatement getMappedStatement() {
-        return (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        if (isProxy()){
+            return (MappedStatement) metaObject.getValue("h.target.delegate.mappedStatement");
+        }else {
+            return (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        }
     }
 
+    /**
+     * 获取StatementHandler中的ParameterHandler
+     * @return
+     */
     public ParameterHandler getParameterHandler() {
-        return (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+        if (isProxy()){
+            return (ParameterHandler) metaObject.getValue("h.target.delegate.parameterHandler");
+        }else {
+            return (ParameterHandler) metaObject.getValue("delegate.parameterHandler");
+        }
     }
 
+    /**
+     * 获取StatementHandler中的BoundSql
+     * @return
+     */
     public void setBoundSql(String sql) {
-        metaObject.setValue("delegate.boundSql.sql",sql);
+        if (isProxy()){
+            metaObject.setValue("h.target.delegate.boundSql.sql",sql);
+        }else {
+            metaObject.setValue("delegate.boundSql.sql",sql);
+        }
+    }
+
+    /**
+     * 是否是代理对象
+     */
+    public boolean isProxy() {
+        return metaObject.hasGetter("h.target");
     }
 }
